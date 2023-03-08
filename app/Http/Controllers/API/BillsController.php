8c6df9e0c5822152cf83d87d9fbe1d32ba0a4d48
Julio Lopez <julzasdf123456@gmail.com>
2023-03-08 13:41:17 +0800

    updates

--- app/Http/Controllers/API/BillsController.php
+++ app/Http/Controllers/API/BillsController.php
@@ -248,6 +248,7 @@ class BillsController extends Controller
                             'BillsExtension.Item24',
                             'PaidBills.NetAmount As NetAmountPaid')
                         ->where('Bills.AccountNumber', $request['q'])
+                        ->orderByDesc('Bills.ServicePeriodEnd')
                         ->take(5)
                         ->get();
 
